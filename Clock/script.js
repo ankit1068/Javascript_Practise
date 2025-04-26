bf8d4b81a9a0ffa585a3a7da908a5ce3df0b71dd
IamAnujYadav <yadavanuj90438@gmail.com>
2025-04-26 11:59:00 +0530

    Live time

---
+++ Clock/script.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+
+    let date = new Date();
+    let options = {
+        weekday: "long", year: "numeric", month: "short",
+        day: "numeric", hour: "2-digit", minute: "2-digit",
+        second:"2-digit"
+    };
+
+    let d = date.toLocaleTimeString("en-us", options);
+
+    time.innerHTML = d
+}, 1000);
