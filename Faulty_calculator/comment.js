f4ae616815ca92d9605c6edc54fa8715ee7dbc97
IamAnujYadav <yadavanuj90438@gmail.com>
2025-04-26 09:55:01 +0530

    calculator

---
+++ Faulty_calculator/comment.js
@@ -0,0 +1,33 @@
+let a = prompt("enter num1")
+let b = prompt("enter num2")
+
+let random = Math.random()
+console.log(random)
+
+let addition = a + b;
+let subtract = a - b;
+let multiple = a * b;
+let divide = a / b;
+
+if (random > 0.1) {
+    // console.log("addition=", a - b)
+    // console.log("divide=", a + b)
+    // console.log("subtract=", a * b)
+    // console.log("multiple=", a / b)
+    alert(`Addition=${a - b}
+           Subtract=${a * b}
+           Multiple=${a / b}
+           Divide=${a + b}`
+    )
+}
+else {
+    // console.log("addition=", a + b)
+    // console.log("subtract=", a - b)
+    // console.log("multiple=", a * b)
+    // console.log("divide=", a / b)
+    alert(`Addition=${a + b}
+           Subtract=${a - b}
+           Multiple=${a * b}
+           Divide=${a / b}`
+    )
+}
